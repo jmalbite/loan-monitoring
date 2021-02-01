475d239bc6214a714a5a2737d1c13842a388903b
jannmichael albite <albite.jannmicheal@intellicare.com.ph>
2021-02-01 20:20:19 +0800

    adjust small adjustments

--- src/components/breakdowns/breakdown.js
+++ src/components/breakdowns/breakdown.js
@@ -6,42 +6,42 @@ function Breakdown() {
       <div className="breakdown">
         <h5>February, 14, 2020</h5>
         <div className="computation">
-          <h4>₱7200</h4>
+          <h4 className="current-balance">₱7200</h4>
           <span>current balance</span>
-          <h4>₱500</h4>
+          <h4 className="amount-paid">₱500</h4>
           <span>amount paid</span>
         </div>
         <div className="remaining-balance">
-          <h4>₱6700</h4>
+          <h4 className="new-balance">₱6700</h4>
           <span>new balance</span>
         </div>
       </div>
 
       <div className="breakdown">
-        <h5>February, 14, 2020</h5>
+        <h5>October, 14, 2020</h5>
         <div className="computation">
-          <h4>₱7200</h4>
+          <h4 className="current-balance">₱7200</h4>
           <span>current balance</span>
-          <h4>₱500</h4>
+          <h4 className="amount-paid">₱500</h4>
           <span>amount paid</span>
         </div>
         <div className="remaining-balance">
-          <h4>₱6700</h4>
+          <h4 className="new-balance">₱6700</h4>
           <span>new balance</span>
         </div>
       </div>
 
       <div className="breakdown">
-        <h5>February, 14, 2020</h5>
+        <h5>March, 04, 2020</h5>
         <div className="computation">
-          <h4>₱7200</h4>
+          <h4 className="current-balance">₱7200</h4>
           <span>current balance</span>
-          <h4>₱500</h4>
+          <h4 className="amount-paid">₱500</h4>
           <span>amount paid</span>
         </div>
         <div className="remaining-balance">
-          <h4>₱6700</h4>
-          <span>₱new balance</span>
+          <h4 className="new-balance">₱6700</h4>
+          <span>new balance</span>
         </div>
       </div>
     </div>
