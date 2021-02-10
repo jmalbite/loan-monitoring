b30083dba34beaf5081ea3d378e7dd89540109e5
jannmichael albite <albite.jannmicheal@intellicare.com.ph>
2021-02-10 08:51:52 +0800

    change ui for breadown

--- src/components/breakdowns/breakdown.js
+++ src/components/breakdowns/breakdown.js
@@ -4,38 +4,12 @@ function Breakdown() {
   return (
     <div className="payment-history">
       <div className="breakdown">
-        <h5>February, 14, 2020</h5>
-        <div className="computation">
+        <h5 className="date">February, 14, 2020</h5>
+        <div className="current-balance">
           <h4 className="current-balance">₱7200</h4>
           <span>current balance</span>
-          <h4 className="amount-paid">₱500</h4>
-          <span>amount paid</span>
         </div>
-        <div className="remaining-balance">
-          <h4 className="new-balance">₱6700</h4>
-          <span>new balance</span>
-        </div>
-      </div>
-
-      <div className="breakdown">
-        <h5>October, 14, 2020</h5>
-        <div className="computation">
-          <h4 className="current-balance">₱7200</h4>
-          <span>current balance</span>
-          <h4 className="amount-paid">₱500</h4>
-          <span>amount paid</span>
-        </div>
-        <div className="remaining-balance">
-          <h4 className="new-balance">₱6700</h4>
-          <span>new balance</span>
-        </div>
-      </div>
-
-      <div className="breakdown">
-        <h5>March, 04, 2020</h5>
-        <div className="computation">
-          <h4 className="current-balance">₱7200</h4>
-          <span>current balance</span>
+        <div className="amount-paid">
           <h4 className="amount-paid">₱500</h4>
           <span>amount paid</span>
         </div>
