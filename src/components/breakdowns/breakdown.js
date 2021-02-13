7482d9afbc7876977db38744d75c73a51a9343e9
jannmichael albite <albite.jannmicheal@intellicare.com.ph>
2021-02-13 18:31:23 +0800

    made some adjustments in breakdown.js

--- src/components/breakdowns/breakdown.js
+++ src/components/breakdowns/breakdown.js
@@ -5,14 +5,17 @@ function Breakdown() {
     <div className="payment-history">
       <div className="breakdown">
         <h5 className="date">February, 14, 2020</h5>
+
         <div className="current-balance">
           <h4 className="current-balance">₱7200</h4>
           <span>current balance</span>
         </div>
+
         <div className="amount-paid">
           <h4 className="amount-paid">₱500</h4>
           <span>amount paid</span>
         </div>
+
         <div className="remaining-balance">
           <h4 className="new-balance">₱6700</h4>
           <span>new balance</span>
